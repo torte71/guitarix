939825a27e1c4e7550a58efd4983917243477993
Thorsten Maerz <info@netztorte.de>
2023-01-29 10:38:54 +0100

    qnd build script + patch

---
+++ trunk/tmbuild.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# pacman -S git base-devel mingw-w64-x86_64-gcc mingw-w64-x86_64-pkg-config mingw-w64-x86_64-meson mingw-w64-x86_64-libsigc++ mingw-w64-x86_64-lv2 mingw-w64-x86_64-eigen3 mingw-w64-x86_64-fftw mingw-w64-x86_64-libffi mingw-w64-x86_64-libsndfile
+
+export PYTHONIOENCODING=utf-8
+export PYTHONLEGACYWINDOWSSTDIO=utf-8
+export WAF_NO_PREFORK=1
+
+
+if [ ! -e .tmpatched ] ; then
+  # unpack waf for patching
+  ./waf --version
+  patch -p1 < tm-04-ugly-Bdynamic-workaround.patch
+  touch .tmpatched
+fi
+
+./waf configure -j 7 	\
+    --check-cxx-compiler="g++" \
+  --no-standalone	\
+  \
+  --no-faust		\
+  --includeconvolver	\
+  --includeresampler	\
+  --no-avahi		\
+  --no-bluez		\
+  --no-nsm		\
+  --no-desktop-update	\
+  --static-lib		\
+  --ldflags="-shared -static $(pkg-config --static --libs cairo) -lfftw3f -liconv"	\
+  --cxxflags="-Wall -DGSEAL_ENABLE -fpermissive -D_USE_MATH_DEFINES" \
+  \
+  && ./waf build \
+  && ./waf install --destdir=$(pwd)/_bin
+
+#  --ldflags="-shared -static -Ld:/c/audio/guitarix-gui/guitarix/trunk/build/src/LV2/xputty $(pkg-config --static --libs cairo) -lfftw3f -liconv "	\
+#  --ldflags="-shared -static -Ld:/c/audio/guitarix-gui/guitarix/trunk/build/src/LV2/xputty $(pkg-config --static --libs cairo) -lfftw3f -liconv -lxcairo"	\
+#  --no-lv2-gui		\
+
+# ./waf build -v | tee build.log
+# ./waf install --destdir=$(pwd)/_bin
+
