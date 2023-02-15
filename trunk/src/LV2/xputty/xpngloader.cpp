bc68fbc0acc0c8c1469d9ee522879c592e302f32
Thorsten Maerz <info@netztorte.de>
2023-02-15 07:14:13 +0100

    revert unnecessary wrapper

--- trunk/src/LV2/xputty/xpngloader.cpp
+++ trunk/src/LV2/xputty/xpngloader.cpp
@@ -131,8 +131,8 @@ void widget_set_icon_from_surface(Widget_t *w, Pixmap *icon_, cairo_surface_t *i
 
 void widget_set_icon_from_png(Widget_t *w, Pixmap *icon_, const unsigned char* name) {
     cairo_surface_t *image = cairo_image_surface_create_from_stream (name);
-    int width, height;
-    os_get_surface_size(w->image, &width, &height);
+    int width = cairo_image_surface_get_width(image);
+    int height = cairo_image_surface_get_height(image);
 #ifdef _WIN32
     Pixmap icon = NULL;
     cairo_surface_t *surface = NULL;
