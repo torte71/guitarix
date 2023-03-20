8e12f6e49cd896dc9dde96307f3f4a3403d92297
Thorsten Maerz <info@netztorte.de>
2023-03-20 14:39:42 +0100

    move compat. definitions into separate header file

--- trunk/src/LV2/xputty/header/xputty.h
+++ trunk/src/LV2/xputty/header/xputty.h
@@ -33,15 +33,12 @@
 
 #include <math.h>
 #include <cairo.h>
-#ifdef _WIN32 //Includes
-#include <windows.h>
-#include <cairo-win32.h>
-#elif __linux__
+#include "xputty-mswin.h" // no ifdef (waf dependency check)
+#ifdef __linux__
 #include <cairo-xlib.h>
 #include <X11/Xutil.h>
 #include <X11/keysym.h>
 #include <X11/Xatom.h>
-#elif _APPLE_
 #endif
 
 #ifdef __cplusplus
@@ -114,110 +111,6 @@ extern "C" {
 				forward declareted structs
 -----------------------------------------------------------------------
 ----------------------------------------------------------------------*/
-
-#ifdef _WIN32 //XCompatDefs
-
-typedef unsigned long int XID;
-typedef unsigned long int Atom;
-typedef XID Display;
-typedef HWND Window;
-typedef XID Pixmap;
-typedef XID KeySym;
-typedef void *XIC;
-typedef void *XIM;
-
-typedef struct {
-  Window window;
-  int x;
-  int y;
-  int button;
-  int keycode;
-  unsigned int state;	/* key or button mask */
-  bool vk_is_final_char; // (from WM_CHAR) readily composed character (after processing dead keys and other input methods)
-  WORD vk;
-} XEvent;
-typedef XEvent XButtonEvent;
-typedef XEvent XKeyEvent;
-typedef XEvent XMotionEvent;
-
-#define Button1 1
-#define Button2 2
-#define Button3 3
-#define Button4 4
-#define Button5 5
-
-#define ShiftMask	(1<<0)
-#define Button1Mask (1<<8)
-
-// xmidi_keyboard.cpp
-#define XK_0	'0'
-#define XK_2	'2'
-#define XK_3	'3'
-#define XK_5	'5'
-#define XK_6	'6'
-#define XK_7	'7'
-#define XK_9	'9'
-#define XK_a	'a'
-#define XK_b	'b'
-#define XK_c	'c'
-#define XK_d	'd'
-#define XK_e	'e'
-#define XK_g	'g'
-#define XK_h	'h'
-#define XK_i	'i'
-#define XK_j	'j'
-#define XK_m	'm'
-#define XK_n	'n'
-#define XK_o	'o'
-#define XK_p	'p'
-#define XK_q	'q'
-#define XK_r	'r'
-#define XK_s	's'
-#define XK_t	't'
-#define XK_u	'u'
-#define XK_v	'v'
-#define XK_w	'w'
-#define XK_x	'x'
-#define XK_y	'y'
-#define XK_z	'z'
-#define XK_space	VK_SPACE // 0x20
-#define XK_quotedbl	'"' //0x22
-#define XK_parenleft	'(' //0x28
-#define XK_plus		'+' //0x2b
-#define XK_comma	',' //0x2c
-#define XK_minus	'-' //0x2d
-#define XK_agrave	0xe0 //'à'
-#define XK_ccedilla	0xe7 //'ç'
-#define XK_eacute	0xe9 //'é'
-#define XK_egrave	0xe8 //'è'
-#define XK_udiaeresis	0xfc //'ü'
-#define XK_dead_circumflex	0xfe52 //0x5e '^'
-#define XK_dead_diaeresis	0xfe57 //0xfc 'ü' - unsure: pass uppercase 'Ü' 0xdc
-#define XK_BackSpace	0xff08
-#define XK_Tab		0xff09
-#define XK_Return	0xff0d
-#define XK_Home		0xff50
-#define XK_Left		0xff51
-#define XK_Up		0xff52
-#define XK_Right	0xff53
-#define XK_Down		0xff54
-#define XK_End		0xff57
-#define XK_Insert	0xff63
-#define XK_KP_Enter	0xff8d
-#define XK_KP_Home	0xff95
-#define XK_KP_Left	0xff96
-#define XK_KP_Up	0xff97
-#define XK_KP_Right	0xff98
-#define XK_KP_Down	0xff99
-#define XK_KP_End	0xff9c
-#define XK_KP_Insert	0xff9e
-#define XK_KP_Add	0xffab
-#define XK_KP_Subtract	0xffad
-
-#define XKeysymToKeycode(A,B) B
-
-#endif
-
 
 /**
  * @brief Childlist_t       - maintain a Widget_t list of childs for a Widget_t
