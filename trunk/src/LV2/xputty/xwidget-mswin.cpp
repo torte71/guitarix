60421af84a4a9883ead070775eb5389dbb96fbea
Thorsten Maerz <info@netztorte.de>
2023-03-21 17:15:12 +0100

    remove unused stuff from platform template

--- trunk/src/LV2/xputty/xwidget-mswin.cpp
+++ trunk/src/LV2/xputty/xwidget-mswin.cpp
@@ -1,17 +1,34 @@
-// vim:ts=4:sw=4:noet:
-#ifndef __GX_PLATFORM_MSWIN_H__
-#define __GX_PLATFORM_MSWIN_H__ 1
-#ifdef _WIN32
+/* vim:ts=4:sw=4:et:
+ *
+ *                           0BSD
+ *
+ *                    BSD Zero Clause License
+ *
+ *  Copyright (c) 2019 Hermann Meyer
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted.
 
-#include "lv2/lv2plug.in/ns/lv2core/lv2.h"
-#include "lv2/lv2plug.in/ns/extensions/ui/ui.h"
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+ * REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+ * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+ * LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+ * OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ * PERFORMANCE OF THIS SOFTWARE.
+ *
+ */
 
-#include <stdio.h>
-#include <windows.h>
+#ifdef _WIN32 //PlatformWrappers
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <windowsx.h>
-#include <cairo-win32.h>
 
-#include <gui/gx_gui.h>
+#include "xwidget.h"
+#include "xwidget_private.h"
 
 // forward declarations
 void SetClientSize(HWND hwnd, int clientWidth, int clientHeight);
@@ -25,10 +42,7 @@ LRESULT onPaint( HWND hwnd, WPARAM wParam, LPARAM lParam );
 -----------------------------------------------------------------------
 ----------------------------------------------------------------------*/
 
-bool gx_gui_open_display(gx_CreamMachineUI *ui) {
-	return true; // STUB
-}
-
+#if 0
 void gx_gui_create_window_and_surface(gx_CreamMachineUI *ui) {
 	// prepare window class
 	static TCHAR szClassName[] = TEXT("gx_CreamMachineUIClass");
@@ -63,40 +77,7 @@ void gx_gui_create_window_and_surface(gx_CreamMachineUI *ui) {
 	// create a permanent surface for drawing (see onPaint() event)
 	ui->surface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, ui->width, ui->height); 
 }
-
-void gx_gui_register_controller_message(gx_CreamMachineUI *ui) {
-	// dummy, not required on MSWin
-}
-
-void gx_gui_destroy_main_window(gx_CreamMachineUI *ui) {
-	DestroyWindow(ui->win);
-	// safe to use: doesnt unregister if there are still windows of this class
-	UnregisterClass(TEXT("gx_CreamMachineUIClass"), NULL);
-}
-
-void gx_gui_resize_surface(gx_CreamMachineUI *ui) {
-	RECT rect;
-
-	GetClientRect(ui->parentWindow, &rect);
-	ui->width = rect.right - rect.left;
-	ui->height = rect.bottom - rect.top;
-	SetClientSize(ui->win, ui->width, ui->height);
-	// image_surface cant be resized (only xlib_surface can)
-	cairo_destroy(ui->cr);
-	cairo_surface_destroy(ui->surface);
-	ui->surface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, ui->width, ui->height); 
-	ui->cr = cairo_create(ui->surface);
-}
-
-void gx_gui_send_controller_event(gx_CreamMachineUI *ui, int controller) {
-	// On X11 this sends a Window-Message (which is received by polling
-	// the eventloop inside the plugins (idle) event_handler).
-	// This could be simulated using a registered Window-Message (in conjunction
-	// with a buffered message polling to have GUI interactions only from
-	// inside the idle event_handler.
-	controller_expose(ui, &ui->controls[controller]);
-	RedrawWindow(ui->win, NULL, NULL, RDW_NOERASE | RDW_INVALIDATE | RDW_UPDATENOW);
-}
+#endif
 
 /*---------------------------------------------------------------------
 -----------------------------------------------------------------------	
@@ -104,69 +85,17 @@ void gx_gui_send_controller_event(gx_CreamMachineUI *ui, int controller) {
 -----------------------------------------------------------------------
 ----------------------------------------------------------------------*/
 
-// map supported key's to integers or return zerro
-static int key_mapping(WPARAM keycode) {
-	// cursor keys currently dont work, as they move the focus to a different control
-	if (keycode == VK_TAB)
-		return (GetKeyState(VK_SHIFT)) ? 1 : 2;
-	else if (keycode == VK_UP)
-		return 3;
-	else if (keycode == VK_RIGHT)
-		return 3;
-	else if (keycode == VK_DOWN)
-		return 4;
-	else if (keycode == VK_LEFT)
-		return 4;
-	else if (keycode == VK_HOME)
-		return 5;
-	else if (keycode == VK_INSERT)
-		return 6;
-	else if (keycode == VK_END)
-		return 7;
-	// keypad
-	else if (keycode == VK_SUBTRACT)
-		return 1;
-	else if (keycode == VK_ADD)
-		return 2;
-	// no separate keycodes for keypad on MSWin
-	/*
-	else if (keycode == VK_KP_UP)
-		return 3;
-	else if (keycode == VK_KP_RIGHT)
-		return 3;
-	else if (keycode == VK_KP_DOWN)
-		return 4;
-	else if (keycode == VK_KP_LEFT)
-		return 4;
-	else if (keycode == VK_KP_HOME)
-		return 5;
-	else if (keycode == VK_KP_INSERT)
-		return 6;
-	else if (keycode == VK_KP_END)
-		return 7;
-	*/
-	else return 0;
-}
-
 /*------------- the event loop ---------------*/
-
-// This is the "idle" function called by the plugin host, where the X11 version
-// polls the messageloop.
-// The messageloop (for subwindows) is implemented as a callback to WndProc,
-// so it cant be polled here.
-// If it becomes necessary to do GUI stuff only from inside the "idle"
-// calls, the Window-Messages might be buffered and processed later/here.
-void event_handler(gx_CreamMachineUI *ui) {
-}
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
+#if 0
 	static double start_value = 0.0;
 	static bool blocked = false;
 	POINT pt;
 
 	// be aware: "ui" can be NULL during window creation (esp. if there is a debugger attached)
-	gx_CreamMachineUI *ui = (gx_CreamMachineUI *)GetWindowLongPtr(hwnd, GWLP_USERDATA);
+	Widget_t *ui = (Widget_t *)GetWindowLongPtr(hwnd, GWLP_USERDATA);
 
 	switch (msg) {
 		// MSWin only: React to close requests
@@ -198,73 +127,47 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 		// X11:ButtonPress
 		case WM_LBUTTONDOWN:
 			if (!ui) return DefWindowProc(hwnd, msg, wParam, lParam);
-			SetCapture(hwnd);
-			ui->pos_x = GET_X_LPARAM(lParam);
-			ui->pos_y = GET_Y_LPARAM(lParam);
-			blocked = true;
-			button1_event(ui, &start_value); // left mouse button click
 			return 0;
 		case WM_MOUSEWHEEL:
 			if (!ui) return DefWindowProc(hwnd, msg, wParam, lParam);
 			// opposed to X11, WM_MOUSEWHEEL doesnt contain mouse coordinates
-			if (GetCursorPos(&pt) && ScreenToClient(hwnd, &pt)) {
-				ui->pos_x = pt.x;
-				ui->pos_y = pt.y;
-			}
-			if (GET_WHEEL_DELTA_WPARAM(wParam) <= 0)
-				scroll_event(ui, -1); // mouse wheel scroll down
-			else
-				scroll_event(ui, 1); // mouse wheel scroll up
+			//if (GetCursorPos(&pt) && ScreenToClient(hwnd, &pt)) {
+			//	ui->pos_x = pt.x;
+			//	ui->pos_y = pt.y;
+			//}
+			//if (GET_WHEEL_DELTA_WPARAM(wParam) <= 0)
+			//	scroll_event(ui, -1); // mouse wheel scroll down
+			//else
+			//	scroll_event(ui, 1); // mouse wheel scroll up
 			return 0;
 		// X11:ButtonRelease
 		case WM_LBUTTONUP:
-			ReleaseCapture();
-			blocked = false;
 			return 0;
 
 		// X11:KeyPress
 		case WM_KEYUP:
 			if (!ui) return DefWindowProc(hwnd, msg, wParam, lParam);
-			switch (key_mapping(wParam)) {
-				case 1: set_previous_controller_active(ui); // "-"
-				break;
-				case 2: set_next_controller_active(ui); // "+"
-				break;
-				case 3: key_event(ui, 1); // UP/RIGHT
-				break;
-				case 4: key_event(ui, -1); // DOWN/LEFT
-				break;
-				case 5: set_key_value(ui, 1); // HOME
-				break;
-				case 6: set_key_value(ui, 2); // INSERT
-				break;
-				case 7: set_key_value(ui, 3); // END
-				break;
-				default:
-				break;
-			}
 			return DefWindowProc(hwnd, msg, wParam, lParam);
 
 		// X11:LeaveNotify (X11:EnterNotify: see WM_MOUSEMOVE)
 		case WM_MOUSELEAVE:
 			if (!ui) return DefWindowProc(hwnd, msg, wParam, lParam);
-			ui->mouse_inside = false;
-			if (!blocked) get_last_active_controller(ui, false);
+			//ui->mouse_inside = false;
 			return 0;
 
 		// X11:MotionNotify
 		case WM_MOUSEMOVE:
 			if (!ui) return DefWindowProc(hwnd, msg, wParam, lParam);
-			if (!ui->mouse_inside) {
-				// emulate X11:EnterNotify
-				ui->mouse_inside = true;
-				if (!blocked) get_last_active_controller(ui, true);
-				SetMouseTracking(ui->win, true); // for receiving (next) WM_MOUSELEAVE
-			}
+			//if (!ui->mouse_inside) {
+			//	// emulate X11:EnterNotify
+			//	ui->mouse_inside = true;
+			//	if (!blocked) get_last_active_controller(ui, true);
+			//	SetMouseTracking(ui->win, true); // for receiving (next) WM_MOUSELEAVE
+			//}
 			// mouse move while button1 is pressed
-			if (wParam & MK_LBUTTON) {
-				motion_event(ui, start_value, GET_Y_LPARAM(lParam));
-			}
+			//if (wParam & MK_LBUTTON) {
+			//	motion_event(ui, start_value, GET_Y_LPARAM(lParam));
+			//}
 			return 0;
 
 		// X11:ClientMessage: not implemented (could be done with WM_USER / RegisterWindowMessage())
@@ -272,9 +175,11 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 		default:
 			return DefWindowProc(hwnd, msg, wParam, lParam);
 	}
+#endif
 }
 
 LRESULT onPaint( HWND hwnd, WPARAM wParam, LPARAM lParam ) {
+#if 0
 	PAINTSTRUCT ps ;
 	gx_CreamMachineUI *ui = (gx_CreamMachineUI *)GetWindowLongPtr(hwnd, GWLP_USERDATA);
 
@@ -304,6 +209,7 @@ LRESULT onPaint( HWND hwnd, WPARAM wParam, LPARAM lParam ) {
 
 	EndPaint( hwnd, &ps );
 	return 0 ;
+#endif
 }
 
 /*---------------------------------------------------------------------
@@ -337,5 +243,9 @@ BOOL SetMouseTracking(HWND hwnd, BOOL enable) {
 /*---------------------------------------------------------------------
 ---------------------------------------------------------------------*/	
 
+
+#ifdef __cplusplus
+}
+#endif
+
 #endif /* _WIN32 */
-#endif /* __GX_PLATFORM_MSWIN_H__ */
