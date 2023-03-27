5c459f6ba5e5355450b8a8d26bc94b6de4208983
Thorsten Maerz <info@netztorte.de>
2023-03-27 11:04:57 +0200

    fix tab: os_open_display

--- trunk/src/LV2/xputty/xwidget-linux.cpp
+++ trunk/src/LV2/xputty/xwidget-linux.cpp
@@ -26,7 +26,7 @@ extern "C" {
 #endif
 
 Display *os_open_display(char *display_name) {
-	return XOpenDisplay(display_name);
+    return XOpenDisplay(display_name);
 }
 
 void os_close_display(Display *dpy) {
